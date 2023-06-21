62301ae4f021f28c6d0996168a7be811ddd3a286
hoangtuhuynh <hoangtuhuynh@cpp.edu>
2023-06-20 19:14:36 -0700

    update

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -12,16 +12,20 @@
 #         self.next = next
 class Solution:
    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        current  = temp = ListNode()
+        head = temp = ListNode()
         while list1 and list2:
             if list1.val <= list2.val:
-                current.next = list1
-                list1, current  = list1.next, list1
+                head.next = list1
+                list1, head = list1.next, list1
             else:
-                current.next = list2
-                list2, current = list2.next, list2
+                head.next = list2
+                list2, head = list2.next, list2
         if list1 or list2:
-            current.next  = list1 if list1 else list2
+                head.next = list1 if list1 else list2
         return temp.next
+        
+
+   
+            
 # @lc code=end
 
