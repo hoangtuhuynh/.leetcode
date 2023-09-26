362074cda187ec18679d8a64967be38c0dd75fdc
hoangtuhuynh <hoangtuhuynh@cpp.edu>
2023-09-26 13:38:13 -0700

    modified sudoku and add clone graph with node and dfs

--- 36.valid-sudoku.py
+++ 36.valid-sudoku.py
@@ -15,7 +15,8 @@ class Solution:
                 if board[r][c] == ".": continue
                 if (board[r][c] in rows[r] or
                 board[r][c] in cols[c] or 
-                board[r][c] in square[r//3, c//3]): return False
+                board[r][c] in square[r//3, c//3]): 
+                    return False
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
                 square[r//3,c//3].add(board[r][c])
