8a98a830a37e45536db7d04af7e1026ee40441ce
hoangtuhuynh <hoangtuhuynh@cpp.edu>
2024-01-05 04:11:07 -0800

    BFS(best) and Iterative DFS

--- 104.maximum-depth-of-binary-tree.py
+++ 104.maximum-depth-of-binary-tree.py
@@ -11,12 +11,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+import collections
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        return 1 + max(self.maxDepth(root.left), 
-                       self.maxDepth(root.right))
-        
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth +1])
+                stack.append([node.right, depth+1])
+        return res
+    
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        q = collections.deque()
+        q.append(root)
+        level = 0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
+
 # @lc code=end
 
