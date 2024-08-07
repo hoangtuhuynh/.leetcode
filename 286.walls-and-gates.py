dc4dd2d326a3ab76c6ab18e64f4f759425203d6c
hoangtuhuynh <hoangtuhuynh@cpp.edu>
2024-08-06 17:17:27 -0700

    graph

--- 286.walls-and-gates.py
+++ 286.walls-and-gates.py
@@ -1,3 +1,31 @@
+import collections
 class Solution:
     def islandsAndTreasure(self, grid: List[List[int]]) -> None:
-        pass
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        q = collections.deque()
+        for i in range(rows):
+            for j in range (cols):
+                if grid[i][j] == 0:
+                    q.append((i,j))
+        self.dfs(grid, rows, cols, q, visited)    
+               
+
+    def dfs(self, grid, rows, cols, q, visited):
+
+
+        
+        while q:
+            
+            current_r, current_c = q.popleft()
+            direction = [[1,0], [-1,0], [0,1], [0,-1]]
+            for dr, dc in direction:
+                row, col  =current_r +dr, current_c+dc
+                if (row in range(rows) and
+                    col in range(cols) and
+                    grid[row][col] == 2147483647 and
+                    grid[row][col] not in visited ):
+                    
+                    grid[row][col] = grid[current_r][current_c]+1
+                    q.append((row,col))
+                    visited.add((row,col))
