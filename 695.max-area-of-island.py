790107bf4aa1af13f873a93055f933f36979e072
hoangtuhuynh <hoangtuhuynh@cpp.edu>
2024-08-06 17:09:59 -0700

    add new graaph

--- 695.max-area-of-island.py
+++ 695.max-area-of-island.py
@@ -1,44 +1,29 @@
-#
-# @lc app=leetcode id=695 lang=python3
-#
-# [695] Max Area of Island
-#
-
-# @lc code=start
 import collections
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows, columns = len(grid), len(grid[0])
-        visited = set()
-        max_area = 0
-        for row in range(rows):
-            for column in range(columns):
-                if (grid[row][column] == 1 and 
-                    (row, column) not in visited):
-                    current_area = 1
-                    current_area = self.dfs(grid, row, column, visited, rows, columns, current_area)
-                    max_area = max(max_area, current_area)
-        return max_area
-                 
-    
-    def dfs(self, grid, row, column, visited, rows, columns, area):
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        rows, cols = len(grid), len(grid[0])
+   
         q = collections.deque()
-        visited.add((row, column))
-        q.append([row, column])
+        for i in range(rows):
+            for j in range (cols):
+                if grid[i][j] == 0:
+                    q.append((i,j))
+        self.dfs(grid, rows, cols, q)    
+               
 
+    def dfs(self, grid, rows, cols, q):
+
+
+        
         while q:
-            r, c = q.popleft()
+            
+            current_r, current_c = q.popleft()
             direction = [[1,0], [-1,0], [0,1], [0,-1]]
             for dr, dc in direction:
-                current_row, current_column = r+dr, c+dc
-                if (current_row in range(rows) and
-                    current_column in range(columns) and
-                    grid[current_row][current_column] == 1 and
-                    (current_row, current_column) not in visited):
-                    area+=1
-                    visited.add((current_row, current_column))
-                    q.append([current_row, current_column])
-
-        return area
-
-# @lc code=end
+                row, col  =current_r +dr, current_c+dc
+                if (row in range(rows) and
+                    col in range(cols) and
+                    grid[row][col] == 2147483647 ):
+                    
+                    grid[row][col] = grid[current_r][current_c]+1
+                    q.append((row,col))
